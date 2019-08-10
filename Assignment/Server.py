7919d6f714473b45d3c9e82182d1339796ce858c
JonathanMichaelEdwards <jon.michael.edwards13@gmail.com>
2019-08-11 10:28:55 +1200

    Socket Assignment

--- Assignment/Server.py
+++ Assignment/Server.py
@@ -1,35 +1,93 @@
-import sys 
-import socket
-
 """
 ...
 """
 
 
-def getPort():
+import time
+import socket
+
+
+
+def currentTime():
     """
-    ...
+    Returns the current time
     """
-    port = int(input("Select a Port Number: "))
+    return time.strftime("%H:%M:%S", time.localtime())
+
+
+
+def acceptSocket(soc):
+    """
+    Printing server acceptance message.
+    """
+    port = soc.getsockname()[1]
+    _, addr = soc.accept()
     
-    # Check port number entered
+    print("{0}  IP = {1}  Port = {2}".format(currentTime(), addr[0], port))
+
+
+
+def fileRequest():
+    """
+    Reading a FileRequest record from the connection.
+    """
+
+
+
+def setupServer():
+    """
+    Checking for errors and setting up the server.
+    """
+    host = ''
+
+    # Analysing port number
+    port = int(input("Select a Port Number: "))
     if port < 1024 and 64000 > port:
         print("ERROR: Port number '{0}' is not within values 1,024 and 64,000.".format(port))
-        sys.exit()
+        exit()
     
-    # Creating and binding socket if no error message detected
-    infoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    a = infoSocket.bind((str(infoSocket.getsockname()), port))
-    ## --- Get hostname from client
+    # Attempting to create a socket
+    try:
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print(str(e))
+        exit()
+    
+    # Attempting to bind to the port number
+    try:
+        soc.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+        exit()
 
+    # Attempting to listen for the socket
+    try:
+        soc.listen(5)
+    except socket.error as e:
+        print(str(e))
+        soc.close()
+        exit()
+    
+    return soc
+
+
+
+def runServer(soc):
+    """
+    Runs the server until closed/exited.
+    """
+    while 1:
+        acceptSocket(soc)
+        fileRequest()
 
 
 
 def main():
     """
-    ...
+    Runs and Controls the program flow of the server.
     """
-    getPort()
+    soc = setupServer()
+    runServer(soc)
 
 
 
