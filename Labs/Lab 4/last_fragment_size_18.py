8904d7bea898e281d5749a70b3d491e5663ce0ed
JonathanMichaelEdwards <jon.michael.edwards13@gmail.com>
2019-08-10 20:54:52 +1200

    merging

---
+++ Labs/Lab 4/last_fragment_size_18.py
@@ -0,0 +1,20 @@
+from math import ceil
+
+
+def last_fragment_size(messageSize_bytes, overheadPerPacket_bytes, maximumNPacketSize_bytes):
+    """
+    Last Fragments. 
+    """
+    s = messageSize_bytes
+    o = overheadPerPacket_bytes
+    m = maximumNPacketSize_bytes
+
+    pktMax = m - o
+    fragments = ceil(s / pktMax)
+    overallPkt = s + o
+    print(fragments)
+    return overallPkt - pktMax * (fragments-1)
+
+
+# print(last_fragment_size(1500, 20, 1500)) # Q)20; Ans = 40
+print(last_fragment_size(10000, 20, 1500)) # Q)22; Ans = 1140
---
+++ Labs/Lab 4/number_fragments_16.py
@@ -0,0 +1,17 @@
+from math import ceil
+
+
+def number_fragments (messageSize_bytes, overheadPerPacket_bytes, maximumNPacketSize_bytes):
+    """
+    Generated Fragments.
+    """
+    s = messageSize_bytes
+    o = overheadPerPacket_bytes
+    m = maximumNPacketSize_bytes
+
+    fragments = s / (m-o)
+
+    return ceil(fragments)
+
+
+# print(number_fragments(10000, 20, 1500)) # Q)21; Ans = 7
