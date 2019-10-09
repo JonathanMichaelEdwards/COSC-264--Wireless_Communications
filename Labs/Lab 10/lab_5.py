c81125035594771f6c4ac4344029cd86695f6e33
Jonathan <jon.michael.edwards13@gmail.com>
2019-10-10 12:54:38 +1300

    updates

--- Labs/Lab 10/lab_5.py
+++ Labs/Lab 10/lab_5.py
@@ -1,19 +1,10 @@
 # Q5 
-# print(hex((0xE34F+0x2396))) # 0x06E6
-print(hex((0xE34F+0x2396 + 1) & 0xFFFF)) # 0x06E6
+print(hex(0xE34F+0x2396 + ((0xE34F+0x2396) >> 16) & 0xFFFF)) # 0x06E6
 
-# Q6, Q7
+# Q6
 s = (0xE34F+0x2396+0x4427+0x99F3)
-print(hex(s+1))  # +1 for the carry
+print((hex(s + ((s) >> 16) & 0xFFFF)))
 
-
-# print(hex(0xFFFF - (s+1)))
-print(hex(~(s+1) & 0xFFFF))
-print(hex(~(s+1) & 0xFFFF))
-
-# Q8
-g = 0b110011
-d = 0b11100011
-
-# print(bin(d % g))
-
+# Q7
+print(hex(~(s + ((s) >> 16) & 0xFFFF) & 0xFFFF))
+print(hex(0xFFFF - (s + ((s) >> 16) & 0xFFFF)))
